0bfaf992b84d8d8c534419870db3b7ddbb906210
David <kantorda@umich.edu>
2018-09-11 17:29:47 -0400

    Created foundational infrastructure

---
+++ PicIN/CVEngine_Wrapper/CVEngine.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "CVEngine_Wrapper.h"
+#include "../CVEngine/CVEngine.h"
+
+using namespace System;
+
+namespace CVEngine
+{
+	public ref class CVEngine : public ManagedWrapper<Core::CVProcessor>
+	{
+	public:
+		CVEngine();
+		void showImage(String^ filePath);
+		const char* infrastructureTest(String^ data);
+	};
+}
---
+++ PicIN/PicIN/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CVEngine;
+
+namespace PicIN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Click += new EventHandler(OnClick);
+        }
+
+        void OnClick(object sender, EventArgs e)
+        {
+            CVEngine.CVEngine cvEngine = new CVEngine.CVEngine();
+
+            string filePath;
+            unsafe
+            {
+                filePath = new string(cvEngine.infrastructureTest(textBox1.Text));
+            }
+
+            cvEngine.showImage(filePath);
+        }
+    }
+}
