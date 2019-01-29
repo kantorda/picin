3b2809ab452e044da3b9aea061233ce953b67b93
David <kantorda@umich.edu>
2019-01-28 20:23:48 -0500

    Clean up from merge

--- Picin/CVEngine/CVProcessor.h
+++ Picin/CVEngine/CVProcessor.h
@@ -7,10 +7,7 @@ namespace Core
 	public:
 		CVProcessor();
 		void showImage(const char* path);
-<<<<<<< HEAD:Picin/CVEngine/CVProcessor.h
-=======
 		void process(const char* path);
 		const char* scanDirectory(const char* path);
->>>>>>> Dev:Picin/CVEngine/CVProcessor.h
 	};
 }
